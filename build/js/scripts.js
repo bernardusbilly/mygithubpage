fde6b45d49697869a823ee18bdb27e07053d5277
Bernardus Billy Tjiptoning <bernardus.billy@hotmail.com>
2016-02-06 23:01:37 -0800

    Update script for backend and frontend section

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -8,7 +8,7 @@ var timeOutToggle = function() {
 
 $(document).ready(function() {
 	
-	$('.parallax').parallax();
+	/*$('.parallax').parallax();*/
 
 	var aboutMe = false;
 
@@ -21,4 +21,98 @@ $(document).ready(function() {
 			aboutMe = false;
 		}
 	});
+
+	var frontend = false;
+	var dropFrontEnd = "400px";
+
+	var activateFrontEnd = function() {
+		frontend = true;
+		$('#backend').css("margin-top", dropFrontEnd);
+		$('#fullstack').css("margin-top", dropFrontEnd);
+		$('#photography').css("margin-top", dropFrontEnd);
+		$('body').animate({
+			scrollTop: $('#frontend').offset().top
+		}, "easein");
+	}
+
+	var deactivateFrontEnd = function() {
+		frontend = false;
+		$('#backend').css("margin-top", "0");
+		$('#fullstack').css("margin-top", "0");
+		$('#photography').css("margin-top", "0");
+	}
+
+	$('#frontend').click(function() {
+		if (frontend == false) {
+			deactivateBackEnd();
+			setTimeout(function() {
+				activateFrontEnd();	
+			}, 300);
+		} else {
+			deactivateFrontEnd();	
+		}
+	});
+
+	var backend = false;
+	var shiftBackEnd = parseInt($('.block').css("width"), 10);
+
+	// always update the block size
+	$(window).resize(function() {
+		shiftBackEnd = parseInt($('.block').css("width"), 10);
+
+		// when backend is active - keep updating the shifted block
+		if (backend == true) {
+			$('#backend').css("left", -shiftBackEnd);
+			$('#fullstack').css("left", -2*shiftBackEnd);
+			$('#photography').css("left", -3*shiftBackEnd);
+		}
+	});
+
+	var activateBackEnd = function() {
+		backend = true;
+		$('#frontend').css("position", "absolute");
+		$('#frontend').css("z-index", "0");
+		$('#frontend').css("left", 0);
+		$('#backend').css("position", "absolute");
+		$('#backend').css("z-index", "10");
+		$('#backend').css("left", -shiftBackEnd);
+		$('#fullstack').css("position", "absolute");
+		$('#fullstack').css("z-index", "0");
+		$('#fullstack').css("left", -2*shiftBackEnd);
+		$('#photography').css("position", "absolute");
+		$('#photography').css("z-index", "0");
+		$('#photography').css("left", -3*shiftBackEnd);
+	}
+
+	var deactivateBackEnd = function() {
+		backend = false;
+		$('#frontend').css("position", "relative");
+		$('#frontend').css("z-index", "0");
+		$('#frontend').css("left", "0");
+		$('#backend').css("position", "relative");
+		$('#backend').css("z-index", "0");
+		$('#backend').css("left", "0");
+		$('#fullstack').css("position", "relative");
+		$('#fullstack').css("z-index", "0");
+		$('#fullstack').css("left", "0");
+		$('#photography').css("position", "relative");
+		$('#photography').css("z-index", "0");
+		$('#photography').css("left", "0");
+	}
+
+	$('#backend').click(function() {
+		if (backend == false) {
+			deactivateFrontEnd();
+			setTimeout(function() {
+				activateBackEnd();	
+			}, 300);
+		} else {
+			deactivateBackEnd();
+		}
+	});
+
+	var activateFullstack = function() {
+
+	}
+
 });
